e73a393adc38a5b83c28a27d18fbc0d9c6640105
Sreeraj <sreerajmp1996@gmail.com>
2024-06-23 00:00:05 +0530

    save image functionality

--- getty_crawler/getty_crawler/spiders/getty_spider.py
+++ getty_crawler/getty_crawler/spiders/getty_spider.py
@@ -1,10 +1,29 @@
 import scrapy
+import os
+from urllib.parse import urlparse
 
 
 class GettySpiderSpider(scrapy.Spider):
     name = "getty_spider"
-    allowed_domains = ["www.gettyimages.in"]
+    allowed_domains = ["www.gettyimages.in",'media.gettyimages.com']
     start_urls = ["https://www.gettyimages.in/photos/aamir-khan-actor"]
 
     def parse(self, response):
-        pass
+        if not os.path.exists('images'):
+            os.makedirs('images')
+
+        for img in response.css('img::attr(src)').getall():
+            if "media.gettyimages.com/id" in img:
+                # Ensure the URL is complete
+                img_url = response.urljoin(img)
+                self.log(f'Found image URL: {img_url}')
+                yield scrapy.Request(img_url, callback=self.save_image)
+    def save_image(self, response):
+        self.log(f'Downloading image from: {response}')
+        # self.log(f'Downloading image from: {response.url}')
+        parsed_url = urlparse(response.url)
+        filename = parsed_url.path.split('/')[-1] 
+        path = os.path.join('images', filename)
+
+        with open(path, 'wb') as f:
+            f.write(response.body)
